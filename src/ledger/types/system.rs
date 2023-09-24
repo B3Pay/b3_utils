590be502cf410a6f9d3379befefb947fc89691d7
b3hr4d <behradmusiclove@yahoo.com>
2023-09-24 14:53:05 +0300

    add bug report system into ledger

--- src/ledger/types/system.rs
+++ src/ledger/types/system.rs
@@ -1,7 +1,8 @@
 use crate::{types::CanisterId, NanoTimeStamp};
-use candid::CandidType;
+use candid::{CandidType, Decode, Encode};
 use ic_cdk::api::management_canister::main::CanisterStatusResponse;
 use serde::{Deserialize, Serialize};
+use std::borrow::Cow;
 
 #[derive(CandidType, Deserialize, Serialize)]
 pub struct SystemCanisterStatus {
@@ -10,4 +11,29 @@ pub struct SystemCanisterStatus {
     pub user_status: usize,
     pub canister_id: CanisterId,
     pub canister_status: CanisterStatusResponse,
+}
+
+#[derive(CandidType, Deserialize, Clone)]
+pub struct Bug {
+    pub name: String,
+    pub description: String,
+    pub logs: Vec<String>,
+    pub version: String,
+    pub canister_id: CanisterId,
+}
+
+#[cfg(feature = "stable_memory")]
+use crate::memory::types::{Bound, Storable};
+
+#[cfg(feature = "stable_memory")]
+impl<'de> Storable for Bug {
+    const BOUND: Bound = Bound::Unbounded;
+
+    fn from_bytes<'a>(bytes: Cow<'a, [u8]>) -> Self {
+        Decode!(bytes.as_ref(), Self).unwrap()
+    }
+
+    fn to_bytes(&self) -> Cow<[u8]> {
+        Cow::Owned(Encode!(self).unwrap())
+    }
 }
