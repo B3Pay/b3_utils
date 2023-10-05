39bae478bf5f9a6cd8f5bee88a6afa03cc712419
b3hr4d <behradmusiclove@yahoo.com>
2023-10-05 16:51:37 +0300

    add doc to util function

--- src/types.rs
+++ src/types.rs
@@ -3,15 +3,26 @@ use std::collections::HashMap;
 use crate::nonce::Nonce;
 use candid::Principal;
 
+/// Represents the ID of a controller in the system.
 pub type ControllerId = Principal;
+
+/// Represents a list of controller IDs.
 pub type ControllerIds = Vec<ControllerId>;
 
+/// Represents the ID of a canister in the system.
 pub type CanisterId = Principal;
+
+/// Represents the ID of a user in the system.
 pub type UserId = Principal;
 
+/// Represents the ID of a role in the system.
 pub type RoleId = Nonce;
 
+/// Represents the ID of an operation in the system.
 pub type OperationId = u64;
+
+/// Represents a deadline timestamp.
 pub type Deadline = u64;
 
+/// Represents metadata as a key-value pair.
 pub type Metadata = HashMap<String, String>;
--- src/utils.rs
+++ src/utils.rs
@@ -6,6 +6,20 @@ use std::fmt;
 pub use encoder::*;
 pub use ic::*;
 
+/// Macro to enforce a condition and return an error if it fails.
+///
+/// # Example
+/// ```rust
+/// use b3_utils::require;
+///
+/// fn example_function(x: u32, y: u32) -> Result<(), String> {
+///     require!(x < y, "x must be less than y");
+///     Ok(())
+/// }
+///
+/// assert_eq!(example_function(1, 2), Ok(()));
+/// assert_eq!(example_function(2, 1), Err("x must be less than y".to_string()));
+/// ```
 #[macro_export]
 macro_rules! require {
     ($condition:expr, $($msg:tt)*) => {
@@ -15,10 +29,43 @@ macro_rules! require {
     };
 }
 
+/// Reports an error by converting it to a string.
+///
+/// # Example
+/// ```
+/// use b3_utils::report;
+///
+/// fn example_function() -> Result<(), String> {
+///    if(true) {
+///       return report("An error occurred");
+///    }
+///
+///    Ok(())
+/// }
+///
+/// assert_eq!(example_function(), Err("An error occurred".to_string()));
+/// ```
 pub fn report<T, E: fmt::Display>(err: E) -> Result<T, String> {
     Err(format!("{}", err))
 }
 
+/// Reports and logs an error (only when the "logging" feature is enabled).
+///
+/// # Example
+/// ```
+/// use b3_utils::{report_log, logs::export_log};
+///
+/// fn example_function() -> Result<(), String> {
+///   if(true) {
+///      return report_log("An error occurred");
+///   }
+///
+///   Ok(())
+/// }
+///
+/// assert_eq!(example_function(), Err("An error occurred".to_string()));
+/// assert_eq!(export_log()[0].message, "An error occurred");
+/// ```
 #[cfg(feature = "logging")]
 pub fn report_log<T, E: fmt::Display>(err: E) -> Result<T, String> {
     crate::log!("{}", err);
@@ -26,6 +73,26 @@ pub fn report_log<T, E: fmt::Display>(err: E) -> Result<T, String> {
     Err(format!("{}", err))
 }
 
+/// Logs and panics with the given error message (only when the "logging" feature is enabled).
+///
+/// # Example
+/// ```rust
+/// use b3_utils::{panic_log, logs::export_log};
+///
+/// fn example_function() -> Result<(), String> {
+///    if(true) {
+///      return panic_log("An error occurred");
+///    }
+///
+///    Ok(())
+/// }
+///
+/// let result = std::panic::catch_unwind(|| example_function());
+///
+/// assert!(result.is_err());
+///
+/// assert_eq!(export_log()[0].message, "An error occurred");
+/// ```
 #[cfg(feature = "logging")]
 pub fn panic_log<T, E: fmt::Display>(err: E) -> T {
     crate::log!("{}", err);
