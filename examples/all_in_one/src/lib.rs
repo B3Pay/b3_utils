e4c9374a69d3cc4e26a08040b62e004b992e3874
b3hr4d <behradmusiclove@yahoo.com>
2023-10-06 11:47:13 +0000

    deploy example to ic

--- examples/all_in_one/src/lib.rs
+++ examples/all_in_one/src/lib.rs
@@ -2,8 +2,8 @@ use b3_utils::http::HttpRequest;
 use ic_cdk::{api::management_canister::http_request::HttpResponse, update};
 
 #[update]
-async fn http_get(url: String, max_response_bytes: Option<u64>) -> String {
-    let request = HttpRequest::new(url).get(max_response_bytes);
+async fn http_post(url: String, json_string: String, max_response_bytes: Option<u64>) -> String {
+    let request = HttpRequest::new(url).post(&json_string, max_response_bytes);
 
     let result = request
         .send_with_closure(|response| HttpResponse {
