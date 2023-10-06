0d157831d1e21ec8dc01e0e5fc857e05704bba21
b3hr4d <behradmusiclove@yahoo.com>
2023-10-06 14:14:35 +0300

    change example

--- examples/all_in_one/src/lib.rs
+++ examples/all_in_one/src/lib.rs
@@ -1,49 +1,26 @@
-use std::cell::RefCell;
+use b3_utils::http::HttpRequest;
+use ic_cdk::{api::management_canister::http_request::HttpResponse, update};
 
-use b3_utils::{
-    memory::{
-        init_stable_mem_refcell,
-        timer::DefaultTaskTimer,
-        types::{Bound, DefaultVMMap, Storable},
-    },
-    Subaccount,
-};
-use candid::CandidType;
-use candid::{decode_one, encode_one};
-use serde::{Deserialize, Serialize};
+#[update]
+async fn http_get(url: String, max_response_bytes: Option<u64>) -> String {
+    let request = HttpRequest::new(url).get(max_response_bytes);
 
-thread_local! {
-    static TASK_TIMER: RefCell<DefaultTaskTimer<Task>> = init_stable_mem_refcell("timer", 1).unwrap();
+    let result = request
+        .send_with_closure(|response| HttpResponse {
+            status: response.status,
+            body: response.body,
+            ..Default::default()
+        })
+        .await;
 
-    static USERS: RefCell<DefaultVMMap<u64, Subaccount>> = init_stable_mem_refcell("users", 2).unwrap();
-}
-
-#[derive(Clone, CandidType, Deserialize, Serialize, Debug, PartialEq, Eq, PartialOrd, Ord)]
-enum Task {
-    AddUser(Subaccount),
-    RemoveUser(u64),
-}
-
-impl Storable for Task {
-    fn from_bytes(bytes: std::borrow::Cow<[u8]>) -> Self {
-        decode_one(&bytes).unwrap()
+    match result {
+        Ok(response) => {
+            String::from_utf8(response.body).expect("Transformed response is not UTF-8 encoded.")
+        }
+        Err(m) => {
+            format!("The http_request resulted in an error. Error: {:?}", m)
+        }
     }
-
-    fn to_bytes(&self) -> std::borrow::Cow<[u8]> {
-        encode_one(self).unwrap().into()
-    }
-
-    const BOUND: Bound = Bound::Unbounded;
 }
 
-#[ic_cdk::update]
-fn add_user() {
-    let principal = ic_cdk::caller();
-
-    USERS.with(|users| {
-        let mut users = users.borrow_mut();
-        let user_id = users.len() as u64;
-
-        users.insert(user_id, principal.into());
-    });
-}
+ic_cdk::export_candid!();
